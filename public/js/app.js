c7075cf8b4a4842462c2cd6095fd3f66a4b17ee3
Wahyu Pratama <wahyulucky28@gmail.com>
2022-01-04 12:08:39 +0700

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -37335,8 +37335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\larastarter\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\larastarter\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\yis-dev\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\yis-dev\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
